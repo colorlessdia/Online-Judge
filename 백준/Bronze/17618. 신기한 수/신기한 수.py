dc729132e7ad21618b5357090a799a72e90535b8
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2023-07-27 00:41:31 +0900

    [Bronze III] Title: 신기한 수, Time: 612 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,7 +1,9 @@
 n = int(input())
+
 cnt = 0
+
 for i in range(1, n + 1):
-    if i % sum(list(map(int, list(str(i))))) == 0:
-        cnt +=1
+    if i % sum([int(c) for c in str(i)]) == 0:
+        cnt += 1
 
 print(cnt)
