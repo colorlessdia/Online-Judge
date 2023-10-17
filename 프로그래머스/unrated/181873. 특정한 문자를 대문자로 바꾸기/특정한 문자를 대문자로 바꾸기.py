bf3e43798cba3139737a5a96ec69563498c23f24
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2023-10-18 08:36:18 +0900

    [level unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -0,0 +1,11 @@
+def solution(my_string, alp):
+    result = ''
+    
+    for c in my_string:
+        if c == alp:
+            result += c.upper()
+        else:
+            result += c
+    
+    return result
+        
