6e0168d5c09885f38177da12c9e0a667137bca58
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2023-10-19 08:58:45 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -0,0 +1,24 @@
+import sys
+
+score_dict = dict(zip('A+ A0 A- B+ B0 B- C+ C0 C- D+ D0 D- F'.split(), map(float, '4.3 4.0 3.7 3.3 3.0 2.7 2.3 2.0 1.7 1.3 1.0 0.7 0.0'.split())))
+
+total = 0
+credit = 0
+
+for _ in range(int(input())):
+    a, b, c = sys.stdin.readline().rstrip().split()
+    
+    credit += float(b)
+    total += float(b) * float(score_dict[c])
+    
+score = total / credit
+
+if len(str(score).split('.')) == 2:
+    d, f = str(score).split('.')
+    
+    if len(f) == 3 and 4 < int(f[2]):
+        print(float(d + '.' + str(int(f[:2]) + 1)))
+    else:
+        print(f'{score:.2f}')
+else:
+    print(f'{score:.2f}')
