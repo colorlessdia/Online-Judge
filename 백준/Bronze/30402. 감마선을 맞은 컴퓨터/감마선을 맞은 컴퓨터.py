74271eecfe8b81406cc8b724417741cb7e1160f4
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2023-11-03 16:18:34 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,12 @@
+pixel = [input().split() for _ in range(15)]
+
+for p in pixel:
+    if 'w' in p:
+        print('chunbae')
+        break
+    elif 'b' in p:
+        print('nabi')
+        break
+    elif 'g' in p:
+        print('yeongcheol')
+        break
