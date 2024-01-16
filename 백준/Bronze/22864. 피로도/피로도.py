20d6e7faac05bc4c9d60e43882d8ce3322fca80e
Kim donghyun <127282051+colorlessdia@users.noreply.github.com>
2024-01-16 14:32:33 +0900

    [Bronze II] Title: 피로도, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.py
@@ -0,0 +1,19 @@
+A, B, C, M = map(int, input().split())
+
+fatigue = 0
+work = 0
+
+for _ in range(24):
+    if M < A:
+        break
+    
+    if fatigue + A <= M:
+        fatigue += A
+        work += B
+    else:
+        fatigue -= C
+
+        if fatigue < 0:
+            fatigue = 0
+
+print(work)
