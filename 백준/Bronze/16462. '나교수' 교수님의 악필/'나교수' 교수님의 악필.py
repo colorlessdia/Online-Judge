bb9356fbdbfee5acf729123f176983ddebdee07c
Kim donghyun <127282051+colorlessdia@users.noreply.github.com>
2024-02-25 02:23:31 +0900

    [Bronze II] Title: '나교수' 교수님의 악필, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/16462. '나교수' 교수님의 악필/'나교수' 교수님의 악필.py
@@ -0,0 +1,31 @@
+import sys
+import math
+
+N = int(input())
+
+total_score = 0
+
+for _ in range(N):
+    score = sys.stdin.readline().rstrip()
+
+    formatted_score = ''
+
+    for number in score:
+        if number in ['0', '6', '9']:
+            formatted_score += '9'
+        else:
+            formatted_score += number
+    
+    if 100 < int(formatted_score):
+        formatted_score = 100
+    else:
+        formatted_score = int(formatted_score)
+    
+    total_score += formatted_score
+
+average_score = total_score / N
+
+if average_score - int(average_score) < 0.5:
+    print(math.floor(average_score))
+else:
+    print(math.ceil(average_score))
