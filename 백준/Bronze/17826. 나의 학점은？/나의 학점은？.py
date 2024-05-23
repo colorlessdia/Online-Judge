816280692686156ab83faed4186b8ba67ca83611
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2024-05-24 01:02:09 +0900

    [Bronze II] Title: 나의 학점은?, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17826. 나의 학점은？/나의 학점은？.py
@@ -0,0 +1,19 @@
+score_list = list(map(int, input().split()))
+score = int(input())
+
+rank = score_list.index(score) + 1
+
+if 49 <= rank <= 50:
+    print('F')
+elif 46 <= rank <= 48:
+    print('C0')
+elif 36 <= rank <= 45:
+    print('C+')
+elif 31 <= rank <= 35:
+    print('B0')
+elif 16 <= rank <= 30:
+    print('B+')
+elif 6 <= rank <= 15:
+    print('A0')
+elif 1 <= rank <= 5:
+    print('A+')
