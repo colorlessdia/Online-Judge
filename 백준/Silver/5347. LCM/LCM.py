6557c05499bc35f2426c7e46ba79a420200c41a1
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2024-06-12 01:07:57 +0900

    [Silver V] Title: LCM, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.py
@@ -0,0 +1,19 @@
+import sys
+
+def GCD(a, b):
+    if b == 0:
+        return a
+
+    return GCD(b, a % b)
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+
+    A, B = max(a, b), min(a, b)
+
+    gcd = GCD(A, B)
+    lcm = int(A * B / gcd)
+
+    print(lcm)
