65f79c324792f77fb79b170b5645762ac0bbf9f0
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2024-07-04 00:34:01 +0900

    [Bronze III] Title: 크림빵, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -1,12 +1,17 @@
 N, K, P = map(int, input().split())
 bread_list = list(map(int, input().split()))
 
+basic_bread = 0
 count = 0
 
-for i in range(0, N * K, K):
-    cream_bread = sum(bread_list[i:i + K])
+for i, bread in enumerate(bread_list):
+    if bread == 0:
+        basic_bread += 1
 
-    if P <= cream_bread:
-        count += 1
+    if i % K == K - 1:
+        if basic_bread < P:
+            count += 1
+
+        basic_bread = 0
 
 print(count)
