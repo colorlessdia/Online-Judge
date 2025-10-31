26749491e4e7d494a649242bafa74de68ca56c6b
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2025-11-01 00:34:28 +0900

    [Bronze IV] Title: 양파 실험, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32369. 양파 실험/양파 실험.py
@@ -0,0 +1,15 @@
+N, A, B = map(int, input().split())
+
+a, b = 1, 1
+
+for _ in range(N):
+    a += A
+    b += B
+
+    if a < b:
+        a, b = b, a
+    
+    if a == b:
+        b -= 1
+
+print(a, b)
