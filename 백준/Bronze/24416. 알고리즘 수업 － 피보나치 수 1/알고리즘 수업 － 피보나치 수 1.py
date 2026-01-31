1454fe40778e79afb22920cde6870957c4a0fae2
Kim Donghyun <127282051+colorlessdia@users.noreply.github.com>
2026-02-01 00:11:58 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+fibonacci = [0] * (N + 1)
+fibonacci[1] = 1
+fibonacci[2] = 1
+
+for i in range(2, N + 1):
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+
+print(fibonacci[N], N - 2)
